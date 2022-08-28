d3954ce1e816b3f3b97cc2925e297bed98d10d84
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-08-28 15:08:01 -0300

    Create Problem1006.java

---
+++ Problem1006.java
@@ -0,0 +1,31 @@
+/*
+Average 2:
+Read three values (variables A, B and C), which are the three student's grades.
+Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5.
+Consider that each grade can go from 0 to 10.0, always with one decimal place.
+
+Input
+The input file contains 3 values of floating points (double) with one digit after the decimal point.
+
+Output
+Print the message "MEDIA"(average in Portuguese) and the student's average according to the following example,
+with a blank space before and after the equal signal.
+ */
+
+import java.util.Scanner;
+
+public class beecrowd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Grade A: ");
+        double A = input.nextDouble();
+        System.out.print("Grade B: ");
+        double B = input.nextDouble();
+        System.out.print("Grade C: ");
+        double C = input.nextDouble();
+
+        double MEDIA = ((A*2)+(B*3)+(C*5))/10;
+
+        System.out.printf("MEDIA = %.1f%n", MEDIA);
+    }
+}
