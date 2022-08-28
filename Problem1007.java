31df0c582857f90208ff87118053beee1b0c0767
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-08-28 15:17:44 -0300

    Create Problem1007.java

---
+++ Problem1007.java
@@ -0,0 +1,34 @@
+/*
+Difference:
+Read four integer values named A, B, C and D. 
+Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+Input
+The input file contains 4 integer values.
+
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, 
+according to the following example, with a blank space before and after the equal signal.
+*/
+
+import java.util.Scanner;
+
+public class beecrowd{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int A = input.nextInt();
+        System.out.print("B: ");
+        int B = input.nextInt();
+        System.out.print("C: ");
+        int C = input.nextInt();
+        System.out.print("D: ");
+        int D = input.nextInt();
+
+        int DIFERENCA = (A * B - C * D);
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+
+    }
+}
