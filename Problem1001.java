5d0a6f668dcf7534c9a67a2fc11a724aeaf22407
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-08-28 14:54:28 -0300

    Update Problem1001.java

--- Problem1001.java
+++ Problem1001.java
@@ -1,3 +1,18 @@
+/*
+Extremely Basic
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. 
+Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, 
+according to the following example.
+
+Obs.: don't forget the endline after all.
+*/
+
 import java.util.Scanner;
 
 public class beecrowd {
