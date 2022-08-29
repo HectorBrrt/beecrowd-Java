d6ca9361ee907617972d1348c29e792f55155248
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-08-29 18:24:35 -0300

    Update Problem1013.java

--- Problem1013.java
+++ Problem1013.java
@@ -1,13 +1,13 @@
 /*
 Greatest:
-Make a program that reads 3 integer values and present the greatest one followed by the message "eh o maior". 
+Make a program that reads 3 integer values and present the greatest one followed by the message "is the greatest". 
 Use the following formula: GreatestAB= (a+b+abs(a-b))/2
 
 Input
 The input file contains 3 integer values.
 
 Output
-Print the greatest of these three values followed by a space and the message “eh o maior”.
+Print the greatest of these three values followed by a space and the message “is the greatest”.
 */
 
 import java.util.Scanner;
