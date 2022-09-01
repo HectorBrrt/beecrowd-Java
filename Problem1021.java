4b4a93c74ba311e2aae3a927413cf40707bb71dd
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-01 16:26:17 -0300

    Create Problem1021.java

---
+++ Problem1021.java
@@ -0,0 +1,13 @@
+/*
+Banknotes and Coins:
+Read a value of floating point with two decimal places. This represents a monetary value.
+After this, calculate the smallest possible number of notes and coins on which the value can be decomposed.
+The considered notes are of 100, 50, 20, 10, 5, 2. The possible coins are of 1, 0.50, 0.25, 0.10, 0.05 and 0.01.
+Print the message “NOTAS:” followed by the list of notes and the message “MOEDAS:” followed by the list of coins.
+
+Input
+The input file contains a value of floating point N (0 ≤ N ≤ 1000000.00).
+
+Output
+Print the minimum quantity of banknotes and coins necessary to change the initial value, as the given example.
+*/
