0393840ed1e3586fb1c76d8392242119fe5add09
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-01 14:58:47 -0300

    Update Problem1019.java

--- Beginner/1000-1019/Problem1019.java
+++ Beginner/1000-1019/Problem1019.java
@@ -1,3 +1,19 @@
+/*
+Time Conversion
+
+Read an integer value corresponding to a person's age (in days) and print it in years, 
+months and days, followed by its respective message “ano(s)”, “mes(es)”, “dia(s)”.
+
+Note: only to facilitate the calculation, consider the whole year with 365 days and 30 days every month. 
+In the cases of test there will never be a situation that allows 12 months and some days, 
+like 360, 363 or 364. This is just an exercise for the purpose of testing simple mathematical reasoning.
+
+Input
+The input file contains 1 integer value.
+
+Output
+Print the output, like the following example.
+*/
 public class beecrowd{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
