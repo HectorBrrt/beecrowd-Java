23a67794af94613812c5f76574d5990b843407b1
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-03 16:59:52 -0300

    Update Problem1003.java

--- Beginner/1000-1019/Problem1003.java
+++ Beginner/1000-1019/Problem1003.java
@@ -10,7 +10,6 @@ The input file contains 2 integer numbers.
 Output
 Print the message "SOMA" with all the capital letters, 
 with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. 
-Like all the problems, don't forget to print the end of line, otherwise you will receive "Presentation Error"
 */
 
 import java.util.Scanner;
