4dcf1fa51a491269dec3d5610a9555565cf2b674
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-03 17:00:36 -0300

    Update Problem1004.java

--- Beginner/1000-1019/Problem1004.java
+++ Beginner/1000-1019/Problem1004.java
@@ -1,7 +1,6 @@
 /*
 Simple Product
 Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
-Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
 
 Input
 The input file contains 2 integer numbers.
