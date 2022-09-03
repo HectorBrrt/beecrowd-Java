3b4eb2637332ac5a701f03bed64023366dbbe358
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-03 16:59:24 -0300

    Update Problem1003.java

--- Beginner/1000-1019/Problem1003.java
+++ Beginner/1000-1019/Problem1003.java
@@ -1,6 +1,5 @@
 /*
 Simple Sum
-
 Read two integer values, in this case, the variables A and B. 
 After this, calculate the sum between them and assign it to the variable SOMA. 
 Write the value of this variable.
