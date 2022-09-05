b91d309a5ede332697bd1a3933dc6e75ff00e07b
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-04 21:42:30 -0300

    Update Problem1015.java

--- Beginner/1000-1019/Problem1015.java
+++ Beginner/1000-1019/Problem1015.java
@@ -26,7 +26,6 @@ public class beecrowd{
         double y2 = input.nextDouble();
 
         double distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-
         System.out.printf("%.4f%n", distance);
     }
 }
