843ff925c072e167ecc8d677bf0e3098c414d47e
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-09 15:17:22 -0300

    Update Problem1008.java

--- Beginner/1000-1019/Problem1008.java
+++ Beginner/1000-1019/Problem1008.java
@@ -3,7 +3,7 @@ Salary:
 Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour.
 Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
 
-Don’t forget the space before and after the equal signal and after the U$.
+Don’t forget the space before and after the equal signal and after the U$. asdfasdfasdfsdf
 
 Input
 The input file contains 2 integer numbers and 1 value of floating point, representing the number,
