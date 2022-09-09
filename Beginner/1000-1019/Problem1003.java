586176e36fbbea1c73eb5c60243ef26c8cca870a
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-09 15:15:13 -0300

    Update Problem1003.java

--- Beginner/1000-1019/Problem1003.java
+++ Beginner/1000-1019/Problem1003.java
@@ -2,10 +2,10 @@
 Simple Sum
 Read two integer values, in this case, the variables A and B. 
 After this, calculate the sum between them and assign it to the variable SOMA. 
-Write the value of this variable.
+Write the value of this variable. 
 
 Input
-The input file contains 2 integer numbers.
+The input file contains 2 integer numbers. 
 
 Output
 Print the message "SOMA" with all the capital letters, 
