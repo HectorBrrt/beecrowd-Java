36c5f6194535101cea67a4f4b81e6d9c89377c76
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-09 15:16:04 -0300

    Update Problem1004.java

--- Beginner/1000-1019/Problem1004.java
+++ Beginner/1000-1019/Problem1004.java
@@ -3,7 +3,7 @@ Simple Product
 Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. 
 
 Input
-The input file contains 2 integer numbers. asdfasdfasdfsdf
+The input file contains 2 integer numbers. 
 
 Output
 Print the message "PROD" and PROD according to the following example, with a blank space before and after the equal signal.
