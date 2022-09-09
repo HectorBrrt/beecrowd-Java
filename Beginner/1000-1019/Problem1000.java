413c9fd75e3aeea4443d959e81050f483fbdc7d2
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-09 15:15:42 -0300

    Update Problem1000.java

--- Beginner/1000-1019/Problem1000.java
+++ Beginner/1000-1019/Problem1000.java
@@ -1,7 +1,7 @@
 /*
 Hello World!
 Welcome to beecrowd!
-Your first program in any programming language is usually "Hello World!".  asdfasdfasdfsdf
+Your first program in any programming language is usually "Hello World!".  
 In this first problem all you have to do is print this message on the screen.
 
 Input
