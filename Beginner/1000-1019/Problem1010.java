176b53dfcbe9e6f932a626182cf47469c96c58d9
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-09 15:17:40 -0300

    Update Problem1010.java

--- Beginner/1000-1019/Problem1010.java
+++ Beginner/1000-1019/Problem1010.java
@@ -6,8 +6,8 @@ the code of a product 2,the number of units of product 2 and the price for one u
 After this, calculate and show the amount to be paid.
 
 Input
-The input file contains two lines of data. In each line there will be 3 values:
-two integers and a floating value with 2 digits after the decimal point.
+The input file contains two lines of data. In each line there will be 3 values: asdfasdfasdfsdf
+two integers and a floating value with 2 digits after the decimal point. 
 
 Output
 The output file must be a message like the following example where "Valor a pagar" means Value to Pay.
