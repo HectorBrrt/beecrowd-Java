ab1c5fb8a1caf25d49f9f0076fcfd5113c771819
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-10 23:47:07 -0300

    Update Problem1015.java

--- Beginner/1000-1019/Problem1015.java
+++ Beginner/1000-1019/Problem1015.java
@@ -1,4 +1,4 @@
-/* test
+/* 
 Distance Between Two Points:
 Read the four values corresponding to the x and y axes of two points in the plane, p1 (x1, y1) and p2 (x2, y2)
 and calculate the distance between them, showing four decimal places after the comma, according to the formula:
