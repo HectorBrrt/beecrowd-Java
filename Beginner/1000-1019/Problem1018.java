4752bd4d88b5b9aee810ed79649c0a9424f42a2c
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-09-10 23:46:18 -0300

    Update Problem1018.java

--- Beginner/1000-1019/Problem1018.java
+++ Beginner/1000-1019/Problem1018.java
@@ -1,4 +1,4 @@
-/*
+/* test
 Banknotes:
 In this problem you have to read an integer value and calculate the smallest possible number
 of banknotes in which the value may be decomposed. The possible banknotes are 100, 50, 20, 10, 5, 2 e 1.
